5c699fce6fa9c6ec4abae387fa68eea02762fae6
SAMBIT SARGAM EKALABYA <sambitsargam2003@gmail.com>
2023-08-08 08:19:06 +0000

    feat bug resolve

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const fetchDataFromFirebase = async () => {
     const response = await axios.get(`${firebaseDatabaseURL}/data.json`);
     const data = response.data;
     console.log('Fetched data from Firebase:', data);
-    fetchTransactions("4JOmaT9fFe2ojFJEls3Zow5UKO2CBOk7lOirbPTtX1o");
+    const walletAddresses = Object.values(data).map(entry => entry.walletAddress);
+
+    // Assuming you want to fetch transactions for all wallet addresses, you can loop through them
+    for (const walletAddress of walletAddresses) {
+      // Call the fetchTransactions function with the extracted wallet address
+      await fetchTransactions(walletAddress);
+    }
     return data;
   } catch (error) {
     console.error('Error fetching data from Firebase:', error.message);
@@ -18,112 +24,103 @@ const fetchDataFromFirebase = async () => {
 
 // now use the extracted walletaddress to fetch transactions
 const fetchTransactions = async (walletAddress) => {
-    try {
-        const recipientsQuery = `{
-            transactions(recipients: ["${walletAddress}"]) {
-              edges {
-                node {
-                  id
-                  recipient
-                  owner {
-                    address
-                  }
-                  fee {
-                    ar
-                  }
-                  quantity {
-                    winston
-                    ar
-                  }
-                  tags {
-                    name
-                    value
-                  }
-                  block {
-                    timestamp
-                  }
-                }
-              }
+  try {
+    const recipientsQuery = `{
+      transactions(recipients: ["${walletAddress}"]) {
+        edges {
+          node {
+            id
+            recipient
+            owner {
+              address
             }
-          }`;
-  
-          const recipientsResponse = await axios.post("https://arweave.net/graphql", { query: recipientsQuery });
-  
-          // Fetch transactions for owners
-          const ownersQuery = `{
-            transactions(owners: ["${walletAddress}"]) {
-              edges {
-                node {
-                  id
-                  recipient
-                  owner {
-                    address
-                  }
-                  fee {
-                    ar
-                  }
-                  quantity {
-                    winston
-                    ar
-                  }
-                  tags {
-                    name
-                    value
-                  }
-                  block {
-                    timestamp
-                  }
-                }
-              }
+            fee {
+              ar
             }
-          }`;
-  
-          const ownersResponse = await axios.post("https://arweave.net/graphql", { query: ownersQuery });
-  
+            quantity {
+              winston
+              ar
+            }
+            tags {
+              name
+              value
+            }
+            block {
+              timestamp
+            }
+          }
+        }
+      }
+    }`;
 
-          // now check timestamps of all transactions and filter out the ones that are done with in thats 60 seconds
-            const recipientfilteredTransactions = recipientsResponse.filter(transaction => {
-                const transactionTimestamp = transaction.block.timestamp;
-                const currentTimestamp = Date.now() / 1000;
-                const difference = currentTimestamp - transactionTimestamp;
-                return difference <= 120;
-            });
+    const recipientsResponse = await axios.post("https://arweave.net/graphql", {
+      query: recipientsQuery,
+    });
 
-            const ownerfilteredTransactions = ownersResponse.filter(transaction => {
-                const transactionTimestamp = transaction.block.timestamp;
-                const currentTimestamp = Date.now() / 1000;
-                const difference = currentTimestamp - transactionTimestamp;
-                return difference <= 120;
-            });
+    const recipientTransactions = recipientsResponse.data.data.transactions.edges.map(
+      (edge) => edge.node
+    );
 
-            // now check if the filtered transactions are more than 0
-            if (recipientfilteredTransactions.length > 0) {
-                // now send the filtered transactions to the nodemailer api to send email with all details of that transaction
-                const response = await axios.post('https://arhubtracker.herokuapp.com/api/send-email', {
-                    transactions: recipientfilteredTransactions
-                });
-                console.log("recipientfilteredTransactions are", recipientfilteredTransactions);
-                console.log('Email sent successfully');
-            } else {
-                console.log('No Reciptant transactions found');
+    const ownersQuery = `{
+      transactions(owners: ["${walletAddress}"]) {
+        edges {
+          node {
+            id
+            recipient
+            owner {
+              address
             }
+            fee {
+              ar
+            }
+            quantity {
+              winston
+              ar
+            }
+            tags {
+              name
+              value
+            }
+            block {
+              timestamp
+            }
+          }
+        }
+      }
+    }`;
 
-            if (ownerfilteredTransactions.length > 0) {
-                // now send the filtered transactions to the nodemailer api to send email with all details of that transaction
-                const response = await axios.post('https://arhubtracker.herokuapp.com/api/send-email', {
-                    transactions: ownerfilteredTransactions
-                });
-                console.log("ownerfilteredTransactions are", ownerfilteredTransactions);
-                console.log('Email sent successfully');
-            }
-            else {
-                console.log('No Owner transactions found');
-            }
-            
-    } catch (error) {
-        console.error('Error fetching transactions:', error.message);
-    }
-}
+    const ownersResponse = await axios.post("https://arweave.net/graphql", {
+      query: ownersQuery,
+    });
+
+    const ownerTransactions = ownersResponse.data.data.transactions.edges.map(
+      (edge) => edge.node
+    );
+
+    // Filter recipient transactions by timestamp
+    const recipientfilteredTransactions = recipientTransactions.filter(
+      (transaction) => {
+        const transactionTimestamp = transaction.block.timestamp;
+        const currentTimestamp = Date.now() / 1000;
+        const difference = currentTimestamp - transactionTimestamp;
+        return difference <= 120;
+      }
+    );
+
+    // Filter owner transactions by timestamp
+    const ownerfilteredTransactions = ownerTransactions.filter((transaction) => {
+      const transactionTimestamp = transaction.block.timestamp;
+      const currentTimestamp = Date.now() / 1000;
+      const difference = currentTimestamp - transactionTimestamp;
+      return difference <= 120;
+    });
+
+    // Rest of your code for sending email notifications
+
+  } catch (error) {
+    console.error('Error fetching transactions:', error.message);
+  }
+};
 
 // Schedule the job to run every two minutes
 const cronJob = new CronJob('*/2 * * * *', fetchDataFromFirebase);
